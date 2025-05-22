e4ab14e04df0d236a7eb4a1e29e9a8de408c2db8
Jack Skjong-Nilsen <jacksnilsen@gmail.com>
2025-05-21 22:27:33 -0500

    Added comments

--- Streams/src/main/java/com/pluralsight/streams/Program.java
+++ Streams/src/main/java/com/pluralsight/streams/Program.java
@@ -8,8 +8,10 @@ public class Program {
 
     public static void main(String[] args) {
 
+        // new list of type person called people
         ArrayList<Person> people = new ArrayList<>();
 
+        // adds a new person to people list
         people.add(new Person("Finn", "Mertens", 17));          // Main human boy
         people.add(new Person("Jake", "the Dog", 30));          // Magic dog
         people.add(new Person("Princess", "Bubblegum", 827));   // Candy kingdom ruler
@@ -21,46 +23,62 @@ public class Program {
         people.add(new Person("Earl", "of Lemongrab", 30));     // Lemongrab
         people.add(new Person("Peppermint", "Butler", 300));    // Dark magic butler
 
+        // prompts user for first or last name and stores it
         System.out.print("Would you like to search by First or Last name? ");
         String choice = userInput.nextLine();
 
+        // if user input equals first or first name
         if (choice.equalsIgnoreCase("first") || choice.equalsIgnoreCase("first name")) {
 
+            // creates a list of first names and puts the person objects into the stream
             List<String> firstNames = people.stream()
+                    // gets the first name of each person object and returns a String
                     .map(person -> person.getFirstName())
+                    // adds all the first names into a list from the stream
                     .toList();
+            // displays each first name
             firstNames.forEach(name -> System.out.println(name));
 
+        // else if user input equals last or last name
         } else if (choice.equalsIgnoreCase("last") || choice.equalsIgnoreCase("last name")) {
 
+            // creates a list of last names and puts the person objects into the stream
             List<String> lastNames = people.stream()
-                    .map(person -> person.getLastName())
+                    // for each person object and returns a last name String
+                    .map(Person::getLastName)
+                    // adds all the last names into a list from the stream
                     .toList();
-            lastNames.forEach(name -> System.out.println(name));
+            // for each last name displays last name
+            lastNames.forEach(System.out::println);
         }
 
         System.out.println("-------------------------------------------------------------");
 
+        // creates a list of person objects and starts the stream
         OptionalDouble averageAge = people.stream()
+                // for each person get their age and return age as an int
                 .mapToInt(person -> person.getAge())
+                // averages of int values
                 .average();
-
-        System.out.println("Average age: " + (int) averageAge.getAsDouble());
+        // displays average age and gets age as an actual double
+        System.out.println("Average age: " + averageAge.getAsDouble());
 
         System.out.println("-------------------------------------------------------------");
 
         OptionalInt oldest = people.stream()
                 .mapToInt(person -> person.getAge())
+                // compares and returns the largest int
                 .reduce(Math::max);
-
+        // displays oldest age and gets the oldest age as an actual int
         System.out.println("Oldest age: " + oldest.getAsInt());
 
         System.out.println("-------------------------------------------------------------");
 
         OptionalInt youngest = people.stream()
-                .mapToInt(person -> person.getAge())
+                .mapToInt(Person::getAge)
+                // compares and returns the smallest int
                 .reduce(Math::min);
-
+        // displays youngest age and gets the youngest age as an actual int
         System.out.println("Youngest age: " + youngest.getAsInt());
     }
 }
